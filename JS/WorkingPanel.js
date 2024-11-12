011805b5c50c7cb033ad50e6d20ceea329fd4ee7
Gabrielleao5563 <92538581+Gabrielleao5563@users.noreply.github.com>
2024-11-12 12:39:58 -0300

    Update Visual
    Mais implementações visuais na página WorkingPanel, trazendo mais o tema do Trello

---
+++ JS/WorkingPanel.js
@@ -0,0 +1,11 @@
+
+
+//Função responsável por criar a Lista quando solicitado
+function CreateList(){
+
+}
+
+//Função que Faz o Texto Aparecer ou Desapareçer quando o usuário clica em uma etiqueta
+function ChangeListPresentingMode(){
+
+}
